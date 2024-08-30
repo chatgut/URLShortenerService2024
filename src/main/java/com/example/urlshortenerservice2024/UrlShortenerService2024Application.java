35d0f0d301c800cb68582d79463826856a62ba0d
LudwigPersson <ludwig.persson@iths.se>
2024-08-30 12:00:00 +0200

    Initial commit with correct files

---
+++ src/main/java/com/example/urlshortenerservice2024/UrlShortenerService2024Application.java
@@ -0,0 +1,13 @@
+package com.example.urlshortenerservice2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerService2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerService2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/urlshortenerservice2024/UrlShortenerService2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshortenerservice2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerService2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
